3570f20cd3a256036a56fbc1721574ec93749eed
vladosimic <46350722+vladosimic@users.noreply.github.com>
2021-03-22 09:38:44 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,171 @@
+export const products = [
+  {
+    id: 1,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/71pomuAsEbL._AC_UX385_.jpg",
+    category: "Men Clothing",
+    title: "Fruit of the Loom Men's Short Sleeve T-Shirt",
+    description:
+      "Short-sleeved crew neck T-shirt.Cotton/Lycra neck with reinforced neck area for extra comfort.97% Cotton, 3% Polyester .",
+    price: 10,
+  },
+  {
+    id: 2,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/91mqP3n1a3L._AC_UX522_.jpg",
+    category: "Men Clothing",
+    title: "Fruit of the Loom Men's Set-in Classic ",
+    description:
+      "80% cotton, 20% polyester^Ribbed waist, neck and cuffs with lycra for shape retention^Tonal embroidery to left chest^Classic fit.80% Cotton, 20% Polyester  ",
+    price: 24,
+  },
+  {
+    id: 3,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81c-dQCT53L._AC_UX466_.jpg",
+    category: "Men Clothing",
+    title: "Levi's Men's 511 Slim' Fit' Jeans ",
+    description:
+      "Sits below waist, slim from hip to ankle.70% Cotton, 28% Lyocell, 2% Elastane ",
+    price: 45,
+  },
+  {
+    id: 4,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/616f7iwNvJL._AC_UY606_.jpg",
+    category: "Men Clothing",
+    title: "SKYTEX UK Mens Fleece Jogging Bottoms Pants",
+    description:
+      "Elasticated wiast, Elasticated Bottom.80% Cotton, 20% Polyester.Side zip Pockets ",
+    price: 32,
+  },
+  {
+    id: 5,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51gxhS-UlpL._AC_UX522_.jpg",
+    category: "Woman Clothing",
+    title: "WearAll Ladies Long Sleeve T-Shirt Top",
+    description:
+      "Authentic & Original Only From WearAll.Length 64cm.95% Viscose, 5% Elastane ",
+    price: 16,
+  },
+  {
+    id: 6,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/61lKki7XhxL._AC_UX385_.jpg",
+    category: "Woman Clothing",
+    title: "Hoerev Women Girls Short High Waist ",
+    description:
+      "As the material is high quality ,the fabric is a bit heavier, Wrinkle resistant, it can be washed well and pleats don't come out. ",
+    price: 22,
+  },
+  {
+    id: 7,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/71cjOWHM3SL._AC_UY445_.jpg",
+    category: "Woman Clothing",
+    title: "WearAll Ladies Palazzo Wide Leg Flared",
+    description:
+      "Authentic & Original Only From WearAll.Avaliable in a range of colours to suit any occasion ",
+    price: 25,
+  },
+  {
+    id: 8,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81oR7tN5WsL._AC_UY445_.jpg",
+    category: "Woman Clothing",
+    title: "Charmnight Women Babydoll Sexy Lingerie ",
+    description:
+      "PREMIUM MATERIAL: 65% cotton+35% polyester. With sexy mesh design, this 100% brand new pure color sexy nightdress adopts water treatment and computer inteligent stitching process. ",
+    price: 30,
+  },
+  {
+    id: 9,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81WZCY9CezL._AC_UX342_.jpg",
+    category: "Woman Clothing",
+    title: "Oversized Hoodie Blanket Women Ultra ",
+    description:
+      "ONE SIZE FITS ALL: This oversized snuggle hoodie blanket combines the convenience of a hoodie, with the warmth and comfort of a blanket, in one easy to manage, stylish piece. This one size fits all hoodie features a giant comfy hood. ",
+    price: 53,
+  },
+  {
+    id: 10,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/61iwebuDGKL._AC_UX425_.jpg",
+    category: "Watches",
+    title: "Mens Digital Sports Watches Military Big",
+    description:
+      "PAIR WITH PEOPLE of DIFFERENT AGES: Watch Case Diameter 50mm, it is fitting to people more than 13 years old, it is also fitting to boys and girls who like big face watch. ",
+    price: 123,
+  },
+  {
+    id: 11,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/61xpfppNVML._AC_UX466_.jpg",
+    category: "Watches",
+    title: "JSDDE Women's Classical Arabic ",
+    description:
+      "DESIGN:This is a classic style watch for women, Arabic numerals dial with pu leather band, no matter for business dress or casual use, it is a good choice. ",
+    price: 140,
+  },
+  {
+    id: 12,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51c79D7VF5L._AC_SX342_.jpg",
+    category: "Software",
+    title: "Windows 10 Professional Key ",
+    description: "Windows 10 Professional Key",
+    price: 19,
+  },
+  {
+    id: 13,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/71thZf5Kx3L._AC_SY445_.jpg",
+    category: "Software",
+    title: "McAfee Total Protection 2021 ",
+    description:
+      "AWARD WINNING ANTIVIRUS SOFTWARE: Defend against viruses and online threats with a combination of cloud based and offline protection for your privacy, identity and your devices ",
+    price: 19,
+  },
+  {
+    id: 14,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/71yZaMJ8ucL._AC_SX450_.jpg",
+    category: "Beauty",
+    title: "Cuticura Original Anti Bacterial Hand Gel",
+    description:
+      "Crisp & fresh.Kills 99% of bacteria.66% Alcohol (by volume).Dermatologically approved. ",
+    price: 6,
+  },
+  {
+    id: 15,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81jj85fctZL._AC_SX425_.jpg",
+    category: "Beauty",
+    title: "Premium Charcoal Face Mask for Men & Women",
+    description:
+      "𝗡𝗔𝗧𝗨𝗥𝗔𝗟 𝗖𝗟𝗔𝗬𝗦 + 𝗢𝗥𝗚𝗔𝗡𝗜𝗖 𝗖𝗛𝗔𝗥𝗖𝗢𝗔𝗟: Our face mask with proprietary 4-clay blend, natural bamboo charcoal and botanical extracts is proven to remove more blackheads and deep-clean pores with gentle exfoliation for a fresh, long-lasting look and feel. ",
+    price: 8,
+  },
+  {
+    id: 16,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/71shE-vAvKL._AC_SX425_.jpg",
+    category: "Beauty",
+    title: "Baylis & Harding Black Pepper and Ginseng Hand Wash for Men",
+    description:
+      "Black pepper and ginseng is the perfect blend of stimulating fruity and spicy top notes with rich base notes of sandalwood and cedar wood ",
+    price: 5,
+  },
+  {
+    id: 17,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51mDKq5wLwL._SX324_BO1,204,203,200_.jpg",
+    category: "Books",
+    title: "American Dirt: The Richard and Judy Book ",
+    description:
+      "Jeanine Cummins s the author of THE OUTSIDE BOY, THE CROOKED BRANCH, the true crime work A RIP IN HEAVEN, and AMERICAN DIRT, all of which are published by Tinder Press.",
+    price: 10,
+  },
+];
